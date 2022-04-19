58c69f86be1d5aa21209b20a9d10eaa08283b49e
ipulotu <ipulotu@byui.edu>
2022-04-19 16:36:23 -0600

    Adding notes for week 1

---
+++ js/main.js
@@ -0,0 +1,42 @@
+
+
+const ol = document.querySelector('ol');
+
+
+function appendurl(url){
+
+    let li = document.createElement('li');
+    let a = document.createElement('a');
+
+    a.setAttribute('href', url.url) ;
+    a.textContent = url.label;
+   
+    li.appendChild(a);
+    ol.appendChild(li);
+
+}
+
+
+const urls = [
+    {
+      label: "Week 1",
+      url: "week1/index.html"
+    },
+    {
+        label: "",
+        url: "week2/index.html"
+    },
+    {
+        label: "",
+        url: "week3/index.html"
+    }
+
+  ]
+
+
+urls.forEach(url => appendurl(url));
+
+
+
+
+
